8a719012303db7deef55fe27823b7a45ff1b4b2d
TerSom <ibnua0291@gmail.com>
2025-11-04 17:00:04 +0700

    belajar node js

---
+++ 17-express-contacts/app.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const expressEjsLayouts = require('express-ejs-layouts')
+const app = express()
+const port = 3000
+
+// gunakan ejs
+app.set('view engine', 'ejs')
+// third party middleware
+app.use(expressEjsLayouts)
+
+// build in middleware
+app.use(express.static("public"))
+
+
+app.get('/', (req, res) => {
+  const mahasiswa = [
+    {
+      nama : "terry",
+      kelas : "PPLG 2"
+    },
+    {
+      nama : "Umay",
+      kelas : "PPLG 2"
+    },
+    {
+      nama : "Somay",
+      kelas : "PPLG 2"
+    },
+  ]
+
+  res.render("index", {
+    name : "terry", 
+    title : "Halaman Home",
+    mahasiswa,
+    layout : 'layouts/main-layout'
+  })
+})
+
+app.get("/about", (req,res,next) => {
+  res.render("about", {
+    title : "Halaman About",
+    layout : 'layouts/main-layout'
+  })
+  // console.log("ini adalah halaman abaout")
+})
+
+app.get("/contact", (req,res) => {
+  res.render("contact", {
+    title : "Halaman Contact",
+    layout : 'layouts/main-layout'
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.use((req,res) => {
+  res.status(404)
+  res.send("<h1>404 NOT FOUND</h1>")
+})
